dbae17b8328846bcfa6ef1224867e0f8244161d5
Felix Asare <flexywork327@gmail.com>
2023-07-03 02:41:00 +0000

    added a _layout file to add StatusBar color as light throught the app

---
+++ app/_layout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+const _layout = ({ children }) => {
+  return (
+    <>
+      <StatusBar style="light" />
+      <Stack screenOptions={{ headerShown: false }}></Stack>
+    </>
+  );
+};
+
+export default _layout;
